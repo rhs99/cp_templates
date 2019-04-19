08d2daba78aa9600efe342ec03ccbaa297749ba6
RakibulHasanSarker <33574220+RakibulHasanSarker@users.noreply.github.com>
2019-04-19 20:18:45 +0600

    Create DSU_on _TREE(n(logn)^2).cpp

---
+++ DSU_on _TREE(n(logn)^2).cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MX 101
+
+int sz[MX];
+int clr[MX];
+vector<int>g[MX];
+map<int,int> *cnt[MX];
+
+void subtree_size(int u,int p)
+{
+    sz[u] = 1;
+    for(int v : g[u])
+    {
+        if(v != p)
+        {
+            subtree_size(v,u);
+            sz[u] += sz[v];
+        }
+    }
+}
+
+void dfs(int u,int p)
+{
+    int mx = -1,big_child = -1;
+    
+    for(int v : g[u])
+    {
+        if(v != p)
+        {
+            if(sz[v] > mx)
+            {
+                mx = sz[v];
+                big_child = v;
+            }
+        }
+    }
+    
+    if(big_child != -1)
+    {
+        cnt[u] = cnt[big_child];
+    }
+    else
+    {
+        cnt[u] = new map<int,int>();
+    }
+    
+    (*cnt[u])[clr[u]]++;
+    
+    for(int v : g[u])
+    {
+        if(v != p && v != big_child)
+        {
+            for(auto &it : *cnt[v])
+            {
+                (*cnt[u])[it.first] += it.second;
+            }
+        }
+    }
+    
+    //now (*cnt[u])[c] is the number of vertices in subtree of vertex u that has color c
+}
+
+int main()
+{
+    return 0;
+}
